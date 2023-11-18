052a5b1d383695c5fac089ab0d0164c8f510aa25
Bảo Võ Hoàng <andy@Baos-MacBook-Pro.local>
2023-11-18 13:39:15 +0100

    code header and about us

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
 import './Header.css';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="#home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavor" />
+      <h1 className="app__header-h1">The Key to Find Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla
+        aspernatur impedit tempore amet aut quas voluptatibus in similique
+        laborum deserunt?
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="img" />
+    </div>
   </div>
 );
 
