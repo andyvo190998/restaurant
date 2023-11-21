e15b3cce48fd457aa088c473f5bffb050e87512f
Bảo Võ Hoàng <andy@Baos-MacBook-Pro.local>
2023-11-21 11:05:52 +0100

    footer

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 import './Newsletter.css';
-
+import { SubHeading } from '../../components';
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter" style={{ zIndex: '1' }}>
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And never miss Latest Updates!</p>
+    </div>
+
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter Your Email Address" />
+      <button className="custom__button">Subscribe</button>
+    </div>
   </div>
 );
 
--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import './Gallery.css';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
+import {
+  BsArrowLeftShort,
+  BsArrowRightShort,
+  BsInstagram,
+} from 'react-icons/bs';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  const scrollRef = useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  const imageArr = [
+    images.gallery01,
+    images.gallery02,
+    images.gallery03,
+    images.gallery04,
+  ];
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: '#AAA', marginTop: '2rem' }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem, accusantium.
+        </p>
+        <button className="custom__button">View More</button>
+      </div>
+
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {imageArr.map((img, idx) => (
+            <div className="app__gallery-images_card" key={idx}>
+              <img src={img} alt="img" />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          ))}
+        </div>
+
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll('left')}
+          />
+          <BsArrowRightShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll('right')}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Gallery;
